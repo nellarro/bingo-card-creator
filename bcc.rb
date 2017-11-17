eba4edec428f1ed10ed2543c5615e556c306fdbc
Jenell Pizarro <nellarro@gmail.com>
2017-11-17 12:58:54 -0500

    pdf shows header

--- bcc.rb
+++ bcc.rb
@@ -32,11 +32,15 @@ BINGO
  
 puts bingo_card
 
-Prawn::Document.generate("bingo.pdf") do
-    define_grid(columns: 5, rows: 6)
+header = ["B", "I", "N", "G", "O"]
 
-    grid(0, 0).bounding_box do
-        stroke_bounds
+Prawn::Document.generate("bingo.pdf") do 
+  define_grid(columns: 5, rows: 6)
+
+  header.each_with_index do | char, i |
+    grid(0, i).bounding_box do
+      stroke_bounds
+      text char, align: :center, valign: :center, size: 50, style: :bold
     end
+  end
 end
-  
